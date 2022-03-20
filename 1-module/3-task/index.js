8cfdb782de895e921bb2fa10c865d055c2fd9409
veretanja <veretanja@yandex.ru>
2022-03-20 16:36:00 +0300

    0-module: 1-task; 1-module: 3-task, 4-task, 5-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,16 @@
 function ucFirst(str) {
   // ваш код...
+  str = str.replace(/ +/g, ' ').trim();
+  switch(str.lenght) {
+		case 0: 
+			str = ''; break;
+		case 1:
+			str = str.charAt(0).toUpperCase();break; 
+		default:
+		  str = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+  }
+  return str;
 }
+  
+
+

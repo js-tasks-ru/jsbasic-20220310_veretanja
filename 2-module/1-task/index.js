fbd960a598c4b29fa5fd51e0af5fdfda5825f3db
veretanja <veretanja@yandex.ru>
2022-03-21 23:32:09 +0300

    2-module: 1-task, 2-task, 3-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+  for (const key in salaries) {
+	  if ( isFinite(salaries[key] ) == true ) {
+		  sum += salaries[key];
+	  } 
+  }
+  return sum;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,13 @@
 let calculator = {
   // ваш код
+	calcA: 0,
+	calcB: 0,
+	read: function(a,b) {
+		this.calcA = a;
+		this.calcB = b;
+  	},
+  	sum:  function() { return this.calcA + this.calcB },  
+	mul:  function() { return this.calcA * this.calcB } 
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ

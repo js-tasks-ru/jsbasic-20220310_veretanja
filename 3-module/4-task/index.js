94a454f83cb0fca799424372ca872553af25146f
veretanja <veretanja@yandex.ru>
2022-03-24 20:14:48 +0300

    3 module: 1, 2, 3, 4, 5 - task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
   // ваш код...
+  let str = '';
+  for (i=0; i < users.length; i++) {
+	  if (users[i].age <= age) {  
+		  str = str + users[i].name + ', ' + users[i].balance + '\n';
+	  }
+  }
+  return str.substring(0,str.length - 1);
+
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
   // ваш код...
+  	let result = {
+		min: 999999999,
+		max: 0
+	}
+	let num = 0;
+	let strSplit = str.split(' ');
+	for (i = 0; i < strSplit.length; i++) {
+		if ( !(isNaN(strSplit[i])) ){  
+			 num = Number(strSplit[i]) ;
+			if (num < result.min) result.min = num;
+			if (num > result.max) result.max = num;
+		}
+	}
+	return result;
 }

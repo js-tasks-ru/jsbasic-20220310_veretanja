fedd2460a6d196e9e448b6da5baba3d0fa382a7f
veretanja <veretanja@yandex.ru>
2022-03-29 21:53:59 +0300

    4 module: 1, 2, 3 - task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+  friends.forEach(elem => {
+	  let liCurr = document.createElement('li');
+	  liCurr.innerHTML = elem.firstName + ' ' + elem.lastName ;
+	  ul.append(liCurr);  
+  } );
+  return ul;
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
-}
+  // ваш код... 
+  let iStatus, iGender, iAge;
+  for (c = 0; c < table.rows[0].cells.length; c++){
+	  if (table.rows[0].cells[c].innerHTML ==`Status`) iStatus = c;
+	  else {
+		  if (table.rows[0].cells[c].innerHTML ==`Gender`) iGender = c;
+		  else {
+			  if (table.rows[0].cells[c].innerHTML ==`Age`) iAge = c;
+		  }
+	  }
+  }
+  for (i = 1; i < table.rows.length; i++) {
+	 if ( table.rows[i].cells[iStatus].hasAttribute('data-available') == false ) { 
+		 table.rows[i].setAttribute('hidden','true');
+	 } else {
+		  if ( table.rows[i].cells[iStatus].getAttribute('data-available') == "true") {
+			  table.rows[i].classList.add('available'); 
+			  } else table.rows[i].classList.add('unavailable');
+	  } 
+	 if ( table.rows[i].cells[iGender].innerText == 'm' ) {
+		table.rows[i].classList.add('male');
+	  } else table.rows[i].classList.add('female');
+	if ( table.rows[i].cells[iAge].innerText < 18 ) {
+		table.rows[i].setAttribute('style',"text-decoration: line-through");
+	}
+  }
+
+} ;
